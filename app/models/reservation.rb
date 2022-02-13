f90cb4cb47d9fb671b59a0b2c3f834460dc20e61
123Nell123 <nellp12321@gmail.com>
2022-02-13 05:10:25 -0800

    init

---
+++ app/models/reservation.rb
@@ -0,0 +1,16 @@
+class Reservation < ApplicationRecord
+    belongs_to :user
+    belongs_to :listing
+
+    def start_date
+        # surcharge start_date
+       if (self.start_date < self.end_date)
+        then 
+         return start_date
+       else 
+        puts "erreur la date de fin est plus petite que la date de debut"
+        break 
+    end
+
+
+end
